8b868048844f388e5acc42829ba1412b2c22dca9
fausio luis <fausioluis@live.com>
2021-05-21 21:18:03 +0200

    movies component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,44 @@
-import { Component } from '@angular/core';
+iimport { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Movie app in angular and .Net Core 5';
+export class AppComponent implements OnInit {
 
-  movies = [
-    {
-      title: 'Man of stiil',
-      releaseDate: new Date(),
-      price: 122.4
-    },
-    {
-      title: 'Batman VS Superman',
-      releaseDate: new Date(),
-      price: 122.4
-    },
-    {
-      title: 'Justice league Zander Cut',
-      releaseDate: new Date(),
-      price: 122.4
-    }
-  ]
+  title = 'Movie app in angular and .Net Core 5';
+  movies;
+
+  ngOnInit(): void {
+
+    setTimeout(() => {
+
+      this.movies = [       ]
+
+      // this.movies = [
+      //   {
+      //     title: 'Man of stiil',
+      //     releaseDate: new Date(),
+      //     price: 122.4
+      //   },
+      //   {
+      //     title: 'Batman VS Superman',
+      //     releaseDate: new Date(),
+      //     price: 122.4
+      //   },
+      //   {
+      //     title: 'Justice league Zander Cut',
+      //     releaseDate: new Date(),
+      //     price: 122.4
+      //   }
+      // ]
+
+    }, 3000);
+
+  }
+
+
+
+
 }
