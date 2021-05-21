fd373340795cc536fb914563913e57278904e6a1
fausio luis <fausioluis@live.com>
2021-05-21 16:30:47 +0200

    creating list of movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-movies-app';
+  title = 'Movie app in angular and .Net Core 5';
+
+  movies = [
+    {
+      title: 'Man of stiil',
+      releaseDate: new Date(),
+      price: 122.4
+    },
+    {
+      title: 'Batman VS Superman',
+      releaseDate: new Date(),
+      price: 122.4
+    },
+    {
+      title: 'Justice league Zander Cut',
+      releaseDate: new Date(),
+      price: 122.4
+    }
+  ]
 }
