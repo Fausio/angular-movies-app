56be804320d5882d5cfc6a5a1237765756d83bd5
fausio luis <fausioluis@live.com>
2021-05-22 08:53:09 +0200

    remove event with splice

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -4,22 +4,23 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
-}) 
+})
 
 export class MoviesListComponent implements OnInit {
 
   title = 'Movie app in angular and .Net Core 5';
 
   @Input()
-  movies:any ;
+  movies: any;
 
   constructor() { }
 
-  ngOnInit(): void {
- 
+  remove(index: number){
+
+    this.movies.splice(index - 1,1)
   }
 
+  ngOnInit(): void {
 
-
-
+  }
 }
