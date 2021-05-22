5f7468a261b764127940e8e11f1d7644d42d3ae8
fausio luis <fausioluis@live.com>
2021-05-22 08:02:03 +0200

    comment  gif page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,12 @@
-iimport { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
   title = 'Movie app in angular and .Net Core 5';
-  movies;
-
-  ngOnInit(): void {
-
-    setTimeout(() => {
-
-      this.movies = [       ]
-
-      // this.movies = [
-      //   {
-      //     title: 'Man of stiil',
-      //     releaseDate: new Date(),
-      //     price: 122.4
-      //   },
-      //   {
-      //     title: 'Batman VS Superman',
-      //     releaseDate: new Date(),
-      //     price: 122.4
-      //   },
-      //   {
-      //     title: 'Justice league Zander Cut',
-      //     releaseDate: new Date(),
-      //     price: 122.4
-      //   }
-      // ]
-
-    }, 3000);
-
-  }
-
-
-
 
 }
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -4,12 +4,44 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
-})
+}) 
+
 export class MoviesListComponent implements OnInit {
+
+  title = 'Movie app in angular and .Net Core 5';
+  movies;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    setTimeout(() => {
+
+      
+
+      this.movies = [
+        {
+          title: 'Man of stiil',
+          releaseDate: new Date(),
+          price: 122.4
+        },
+        {
+          title: 'Batman VS Superman',
+          releaseDate: new Date(),
+          price: 122.4
+        },
+        {
+          title: 'Justice league Zander Cut',
+          releaseDate: new Date(),
+          price: 122.4
+        }
+      ]
+
+    }, 3000);
+
   }
+
+
+
 
 }
