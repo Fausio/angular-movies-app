4d9d91eb86b7c0de457c94968f7f0453fba51a70
fausio luis <fausioluis@live.com>
2021-05-23 11:29:19 +0200

    ng class dynamic appy css

--- src/app/utilities/rating/rating.component.ts
+++ src/app/utilities/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -9,7 +9,39 @@ export class RatingComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  maxRating = 5;
+  maxRatingArr: any;
+
+  selectedRate = 0;
+  priviosRate = 0;
+
+
+
   ngOnInit(): void {
+
+    this.maxRatingArr = Array(this.maxRating).fill(0);
+
+  }
+
+  handleMauseEnter(index: number) {
+    this.selectedRate = index + 1;
+  }
+
+  handleMouseLeave() {
+
+    if (this.selectedRate !== 0) {
+      this.selectedRate = this.priviosRate;
+    } else {
+      this.selectedRate = 0;
+    }
+
+
+  }
+
+  rate(index: number) {
+    this.selectedRate = index + 1;
+    this.priviosRate  = this.selectedRate;
   }
 
 }
