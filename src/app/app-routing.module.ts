88ad88ead1c7affdcd4941c04505c5bd9cc76290
fausio luis <fausioluis@live.com>
2021-06-04 19:55:08 +0200

    rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AtorCreateComponent } from './ator/ator-create/ator-create.component';
+import { AtorIndexComponent } from './ator/ator-index/ator-index.component';
 import { GenreCreateComponent } from './genre/genre-create/genre-create.component';
 import { GenreIndexComponent } from './genre/genre-index/genre-index.component';
 import { HomeComponent } from './home/home.component';
+import { MovietheaterCreateComponent } from './movie-theater/movietheater-create/movietheater-create.component';
+import { MovietheaterIndexComponent } from './movie-theater/movietheater-index/movietheater-index.component';
+import { MovieCreateComponent } from './movies/movie-create/movie-create.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
+  
   { path:'genre', component:GenreIndexComponent},
-  { path:'genre-create', component:GenreCreateComponent}
+  { path:'genre/create', component:GenreCreateComponent},
 
+  { path:'ator', component:AtorIndexComponent},
+  { path:'ator/create', component:AtorCreateComponent},
+
+  { path:'movietheater', component:MovietheaterIndexComponent},
+  { path:'movietheater/create', component:MovietheaterCreateComponent},
+
+  { path:'movie/create', component:MovieCreateComponent} 
 ];
 
 @NgModule({
