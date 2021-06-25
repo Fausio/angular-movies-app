46d499274221dedd6f4e0965c258f8a5f27a467a
fausio luis <fausioluis@live.com>
2021-06-25 22:31:16 +0200

    Reacting to Changes in the Form - Movie Filter Component

--- src/app/movies/filter/filter.component.ts
+++ src/app/movies/filter/filter.component.ts
@@ -12,7 +12,42 @@ export class FilterComponent implements OnInit {
 
   form: FormGroup | any  ;
 
-  filterMovies: any;
+  filterMovies = [
+    {
+      title: 'Man of stiil',
+      releaseDate: new Date(),
+      price: 122.4,
+      poster : "https://image.tmdb.org/t/p/w154/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg"
+    },
+    {
+      title: 'Batman VS Superman',
+      releaseDate: new Date(),
+      price: 122.5,
+      poster : "https://image.tmdb.org/t/p/w154/zO5913BWcWYrhgACNpaaw9CCycX.jpg"
+    },
+    {
+      title: 'Justice league Zander Cut',
+      releaseDate: new Date(),
+      price: 122.4,
+      poster:"https://image.tmdb.org/t/p/w154/kn72J6BFcN71VYOl8sTVeo7x9ph.jpg"
+      
+    },
+    {
+      title: 'Batman VS Superman',
+      releaseDate: new Date(),
+      price: 122.4,
+      poster: "https://image.tmdb.org/t/p/w154/w8Hi3GI4q1oR6EImrDWrAQFn8Ha.jpg"
+    },
+    {
+      title: 'Justice league Zander Cut',
+      releaseDate: new Date(),
+      price: 122.4,
+      poster: "https://image.tmdb.org/t/p/w154/qDA95ebiy3W3m8hTRB3xZNZVVBM.jpg"
+ 
+    } 
+  ]
+
+  originalFilterMovies = this.filterMovies;
 
   genres = [
     { id: 1, Name: 'Farowest' },
@@ -22,41 +57,7 @@ export class FilterComponent implements OnInit {
 
 
   ngOnInit(): void {
-   
-    this.filterMovies = [
-      {
-        title: 'Man of stiil',
-        releaseDate: new Date(),
-        price: 122.4,
-        poster : "https://image.tmdb.org/t/p/w154/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg"
-      },
-      {
-        title: 'Batman VS Superman',
-        releaseDate: new Date(),
-        price: 122.5,
-        poster : "https://image.tmdb.org/t/p/w154/zO5913BWcWYrhgACNpaaw9CCycX.jpg"
-      },
-      {
-        title: 'Justice league Zander Cut',
-        releaseDate: new Date(),
-        price: 122.4,
-        poster:"https://image.tmdb.org/t/p/w154/kn72J6BFcN71VYOl8sTVeo7x9ph.jpg"
-        
-      },
-      {
-        title: 'Batman VS Superman',
-        releaseDate: new Date(),
-        price: 122.4,
-        poster: "https://image.tmdb.org/t/p/w154/w8Hi3GI4q1oR6EImrDWrAQFn8Ha.jpg"
-      },
-      {
-        title: 'Justice league Zander Cut',
-        releaseDate: new Date(),
-        price: 122.4,
-        poster: "https://image.tmdb.org/t/p/w154/qDA95ebiy3W3m8hTRB3xZNZVVBM.jpg"
-   
-      } 
-    ]
+    
 
     this.form = this.formBuilder.group({
 
@@ -68,13 +69,20 @@ export class FilterComponent implements OnInit {
  
     this.form.valueChanges
     .subscribe((values: any) => {
-      console.log(values);
+     this.filterMovies = this.originalFilterMovies;
+     this.filterMoviesMethod(values);
     });
 
 
 
   }
 
+  filterMoviesMethod(value: any){
+      if(value.Title){
+          this.filterMovies = this.filterMovies.filter(x => x.title.indexOf(value.Title) !== -1);
+      }
+
+  }
   clearForm(){
 
   }
